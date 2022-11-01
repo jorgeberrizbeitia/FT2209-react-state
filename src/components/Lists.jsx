f5c2455705448ef1242606b2b4160201a700c1da
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-11-01 11:00:58 +0100

    lists done

---
+++ src/components/Lists.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+// rfce para crear la plantilla de un componente funcional
+
+function Lists() {
+
+  const someArr = ["Hola", "Hello", "Halo", "Bonjour"]
+
+  // const someJSXelements = [<li>Hola</li>, <li>Hello</li>, <li>Halo</li>, <li>Bonjour</li>]
+
+  const booksArr = [
+    { _id: 123, title: "El Hobbit", author: "Tolkien" },
+    { _id: 999, title: "El Hobbit", author: "Tolkien" },
+    { _id: 456, title: "The Clone Wars", author: "No lo sé" }
+  ]
+
+  return (
+    <div>
+      
+      <h3>Listas</h3>
+
+      <ul>
+        {someArr.map((eachElement) => {
+          return (
+            <li key={eachElement}>{eachElement}</li>
+          )
+        })}
+
+        {/* {someArr.map((eachElement) => <li>{eachElement}</li>)} */}
+      </ul>
+      
+      
+        {booksArr.map((eachElement) => {
+          return (
+            <div key={eachElement._id} className="card">
+              <h5>{eachElement.title}</h5>
+              <p>{eachElement.author}</p>
+            </div>
+          )
+        })}
+      
+
+    </div>
+  )
+}
+
+export default Lists
